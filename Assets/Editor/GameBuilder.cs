472c5fb7472b5b24f7716984d56d7b6a58aecedd
danielszweda <daniel.szweda@vrjam.com>
2022-12-01 09:24:21 +0000

    Added Build File

---
+++ Assets/Editor/GameBuilder.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class GameBuilder : MonoBehaviour
+{
+    [MenuItem("Build/Build Windows")]
+    public static void BuildWindows()
+    {
+        var buildPlayerOptions = new BuildPlayerOptions()
+        {
+            scenes = new[]{"Assets/Scenes/SampleScene.unity"},
+            locationPathName = "Builds/Windows/vrjam.exe",
+            target = BuildTarget.StandaloneWindows64,
+            options = BuildOptions.None
+        };
+
+        var buildReport = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        var summary = buildReport.summary;
+        switch (summary.result)
+        {
+            case BuildResult.Succeeded:
+                Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+                break;
+            case BuildResult.Failed:
+                Debug.LogError("Build Failed");
+                break;
+            case BuildResult.Unknown:
+                Debug.LogError("Build Unknown");
+                break;
+            case BuildResult.Cancelled:
+                Debug.LogWarning("Build Cancelled");
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        
+    }
+    
+}
